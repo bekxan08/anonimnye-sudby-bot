4aa1188d7278330a399652c53ceea60141836d99
bekxan08 <bekxan0812@gmail.com>
2026-02-17 09:49:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,21 +6,23 @@ from database import init_db
 from users import user_router
 from admin import admin_router
 from payment import pay_router
+from chat import chat_router  # ИМПОРТ НОВОГО МОДУЛЯ
 
-# Регистрация модулей
+# Регистрация всех модулей
 dp.include_router(user_router)
 dp.include_router(admin_router)
 dp.include_router(pay_router)
+dp.include_router(chat_router) # ПОДКЛЮЧЕНИЕ ЧАТА
 
 app = Flask('')
 @app.route('/')
-def home(): return "Online"
+def home(): return "Бот работает"
 def run(): app.run(host='0.0.0.0', port=8080)
 
 async def start():
     await init_db()
     Thread(target=run).start()
-    print("Бот запущен!")
+    print("Бот и все его модули запущены!")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
