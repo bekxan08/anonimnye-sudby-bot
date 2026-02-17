09a4f4c08a9791e9cc17f01400715078736ec6b6
bekxan08 <bekxan0812@gmail.com>
2026-02-17 09:47:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 import asyncio
+from flask import Flask
+from threading import Thread
 from config import bot, dp
 from database import init_db
 from users import user_router
 from admin import admin_router
 from payment import pay_router
-from flask import Flask
-from threading import Thread
 
-# Подключаем все модули (роутеры)
+# Регистрация модулей
 dp.include_router(user_router)
 dp.include_router(admin_router)
 dp.include_router(pay_router)
 
-# Flask для Uptime
 app = Flask('')
 @app.route('/')
-def home(): return "Бот работает"
+def home(): return "Online"
 def run(): app.run(host='0.0.0.0', port=8080)
 
-async def main():
+async def start():
     await init_db()
-    Thread(target=run).start() # Keep alive
+    Thread(target=run).start()
+    print("Бот запущен!")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(start())
